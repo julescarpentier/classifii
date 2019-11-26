1938de1af00e0235eed493d2751e5ebeecf0aeb6
Jules Carpentier <jules.carpentier@gmail.com>
2019-11-26 12:41:00 +0100

    Dataframe like aide 1

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
 import pandas as pd
 import nltk.tokenize as nltk
-import nltk.tokenize.WordPunctTokenizer as wpt
 from os import listdir
 from os.path import isfile, join
 
 labels = [
     'alt.atheism',
-    'comp.graphics',
-    'comp.os.ms-windows.misc',
-    'comp.sys.ibm.pc.hardware',
-    'comp.sys.mac.hardware',
-    'comp.windows.x',
-    'misc.forsale',
-    'rec.autos',
-    'rec.motorcycles',
-    'rec.sport.baseball',
-    'rec.sport.hockey',
-    'sci.crypt',
-    'sci.electronics',
-    'sci.med',
-    'sci.space',
-    'soc.religion.christian',
-    'talk.politics.guns',
-    'talk.politics.mideast',
-    'talk.politics.misc',
-    'talk.religion.misc'
+    # 'comp.graphics',
+    # 'comp.os.ms-windows.misc',
+    # 'comp.sys.ibm.pc.hardware',
+    # 'comp.sys.mac.hardware',
+    # 'comp.windows.x',
+    # 'misc.forsale',
+    # 'rec.autos',
+    # 'rec.motorcycles',
+    # 'rec.sport.baseball',
+    # 'rec.sport.hockey',
+    # 'sci.crypt',
+    # 'sci.electronics',
+    # 'sci.med',
+    # 'sci.space',
+    # 'soc.religion.christian',
+    # 'talk.politics.guns',
+    # 'talk.politics.mideast',
+    # 'talk.politics.misc',
+    # 'talk.religion.misc'
 ]
 
-df = pd.DataFrames()
+df = pd.DataFrame()
 
 for label in labels:
-    dir = '../20news-18828/' + label
-    for file in listdir(dir):
-        file_path = join(dir, file)
+    dir_path = 'data/20news-18828/' + label
+    target = labels.index(label)
+    for file in listdir(dir_path):
+        file_path = join(dir_path, file)
         if isfile(file_path):
             with open(file_path, 'r', encoding='iso-8859-1') as f:
                 s = f.read()
-                # tokens = wpt.tokenize(s)
-                print(s)
+                tokens = nltk.WordPunctTokenizer().tokenize(s)
+                df = df.append(
+                    pd.DataFrame({'text': s, 'label': label, 'target': target, 'tokens': [tokens]}, index=[file]))
+
+print(df)
