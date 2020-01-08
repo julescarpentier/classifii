80da1741035fce22d09c5498b0f407427bb3d17d
yanni.blier <yanni.blier@telecom-sudparis.eu>
2020-01-08 12:53:55 +0100

    Merge branch 'master' of C:\Users\YANNI\classifii with conflicts.

--- notebooks/rationales.py
+++ notebooks/rationales.py
@@ -4,12 +4,13 @@ import os
 
 import numpy as np
 import tensorflow as tf
+from random import random
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.utils import to_categorical
 
-from justifii.models import Text, Label
-from models import fully_conv_with_rationales, fully_conv_without_rationales
+from models import fully_conv_rationales
+from utilities.dataset import get_texts_labels
 from utilities.embedding import get_embedding_matrix, get_pre_trained_trainable_embedding_layer
 from utilities.plotting import plot_acc, plot_loss
 
@@ -21,19 +22,19 @@ VALIDATION_SPLIT = 0.2
 
 # Prepare text samples and their labels
 
-print('Retrieving texts from database')
+print('Processing text dataset')
 
-texts = []
-labels = []
-nb_labels = Label.query.count()
-rationales = []
-for text in Text.query.filter(Text.rationales.any()):
-    texts.append(text.get_content())
-    labels.append(text.label.target)
-    rationales.append(text.get_r(nb_labels, MAX_SEQUENCE_LENGTH))
-rationales = np.array(rationales)
+texts, labels, labels_index = get_texts_labels()
 
-print('Retrieved {} texts.'.format(len(texts)))
+print('Found {} texts.'.format(len(texts)))
+
+# fake rationales
+rationales = np.zeros((len(texts), len(labels_index), MAX_SEQUENCE_LENGTH))
+for i in range(rationales.shape[0]):
+    j = labels[i]
+    for k in range(rationales.shape[2]):
+        if random() < 0.03:
+            rationales[i][j][k] = 1
 
 # Vectorize the text samples into a 2D integer tensor
 tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
@@ -53,7 +54,7 @@ indices = np.arange(data.shape[0])
 np.random.shuffle(indices)
 data = data[indices]
 labels = labels[indices]
-rationales = rationales[indices]
+# rationales = rationales[indices]
 num_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
 
 # split the data into a training set and a validation set
@@ -73,15 +74,11 @@ embedding_matrix = get_embedding_matrix(num_words, MAX_NUM_WORDS, word_index)
 # Embedding layer
 embedding_layer = get_pre_trained_trainable_embedding_layer(num_words, embedding_matrix, MAX_SEQUENCE_LENGTH)
 
-print('Training models.')
+print('Training model.')
 
-model_with_rationales = fully_conv_with_rationales.get_compiled_model(embedding_layer, MAX_SEQUENCE_LENGTH, nb_labels)
-model_without_rationales = fully_conv_without_rationales.get_compiled_model(embedding_layer, MAX_SEQUENCE_LENGTH,
-                                                                            nb_labels)
-history_with_rationales = model_with_rationales.fit(x_train, (y_train, r_train), batch_size=128, epochs=10,
-                                                    validation_data=(x_val, (y_val, r_val)))
-history_without_rationales = model_without_rationales.fit(x_train, y_train, batch_size=128, epochs=10,
-                                                          validation_data=(x_val, y_val))
+model = fully_conv_rationales.get_compiled_model(embedding_layer, MAX_SEQUENCE_LENGTH, len(labels_index))
+
+history = model.fit(x_train, (y_train, r_train), batch_size=32, epochs=10, validation_data=(x_val, (y_val, r_val)))
 
 # ensure the output folder exists
 try:
@@ -90,7 +87,7 @@ except OSError:
     pass
 
 # Plot accuracy
-plot_acc('./output/fully_conv_with_rationales_acc.png', history_with_rationales)
+plot_acc('./output/fully_conv_rationales_acc.png', history)
 
 # Plot trainable, pre-trained and both losses
-plot_loss('./output/fully_conv_with_rationales_loss.png', history_with_rationales)
+plot_loss('./output/fully_conv_rationales_loss.png', history)
